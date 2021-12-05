51746d88c13358969e65f9266ad8071084a705a3
Daniil <92120469+DaniilSHA@users.noreply.github.com>
2021-12-05 12:46:44 +0300

    commit with mods

--- src/StringService.java
+++ src/StringService.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Objects;
 
 public class StringService {
@@ -7,27 +9,11 @@ public class StringService {
     public static String[] sort(String[] as, boolean dir) throws StringArrayException {
         if ((as == null) || (as.length == 0))
             throw new StringArrayException("ссылка на исходный массив = null или он пуст");
-        String temp;
+
         if (dir) {
-            for (int i = 1; i < as.length; i++) {
-                int j = i - 1;
-                temp = as[i];
-                while (j >= 0 && as[j].toLowerCase().charAt(0) >= temp.toLowerCase().charAt(0)) {
-                    as[j + 1] = as[j];
-                    j--;
-                }
-                as[j + 1] = temp;
-            }
+            Arrays.sort(as);
         } else {
-            for (int i = 1; i < as.length; i++) {
-                int j = i - 1;
-                temp = as[i];
-                while (j >= 0 && as[j].toLowerCase().charAt(0) < temp.toLowerCase().charAt(0)) {
-                    as[j + 1] = as[j];
-                    j--;
-                }
-                as[j + 1] = temp;
-            }
+            Arrays.sort(as, Comparator.reverseOrder());
         }
         return as;
     }
